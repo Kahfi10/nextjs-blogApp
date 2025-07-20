6e25a68aa0cba9ebd533a88fd1175358ee00d561
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-07-20 17:18:56 +0800

    feat: add MorphingDialog component with trigger, content, and close functionality; implement useClickOutside hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import SectionPhoto from "../components/SectionPhoto";
+import  DetailBlog  from "../components/detail-blog";
 
 export default function Home() {
   return (
     <>
         <Header />
         <SectionPhoto />
+        <DetailBlog />
     </>
   );
 }
